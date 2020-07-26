6cb90faac0d2c0a95411310d684e2926f0069e04
Pablo Vera Terán <pabloverat@MacBook-Air-de-Pablo.local>
2020-07-26 13:59:38 -0500

    basic struct Time class

---
+++ Time.java
@@ -0,0 +1,19 @@
+public class Time{
+
+	/**/
+	private int hour;
+	private int minute;
+	private int second;
+	private int milisecond;
+
+	/**/
+	public Time(){
+		this.hour = 0;
+		this.minute = 0;
+		this.second = 0;
+		this.milisecond = 0;
+	}
+
+
+
+}
