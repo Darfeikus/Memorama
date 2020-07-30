ac5af495c725247a0d80a37e047b2118ce04c243
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2020-07-29 19:10:47 -0600

    Basic Simulation structure

---
+++ Simulation.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Simulation {
+    Scanner scanner = new Scanner(System.in);
+    Time time;
+    RAM ram;
+    VRAM vram;
+    // List<Block> blockList = new ArrayList<>();
+    // Log log = new Log();
+    int currentPageFaults = 0;
+    int[] swaps = new int[2]; // 0 for out y 1 for in
+
+    void readEntry(String command) {
+        // Reads commands, pseudo main
+    }
+
+    void createProcess(int processId, int processSize) {
+
+    }
+}
