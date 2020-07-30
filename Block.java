48c593fe48c0a70824633499ce88676e22457250
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2020-07-29 19:31:36 -0600

    Basic Struct, de-frenched Bloque into Block

--- Block.java
+++ Block.java
@@ -2,15 +2,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 //Class used to encapsulate information about each block of processes
-public class Bloque {
-    private List<Process> listOfProcesses; //List of processes in each block
-    private int swapOut; //Number of swap outs made
-    private int swapIn; //Number of swap in made
-    private Time startTime; //Time in which the block starts
-    private Time endTime; //Time in which the block ends
+public class Block {
+    private List<Process> listOfProcesses; // List of processes in each block
+    private int swapOut; // Number of swap outs made
+    private int swapIn; // Number of swap in made
+    private Time startTime; // Time in which the block starts
+    private Time endTime; // Time in which the block ends
 
-    //Constructor
-    public Bloque(listOfProcesses, swapOut, swapIn, startTime, endTime) {
+    // Constructor
+    public Block(List<Process> listOfProcesses, int swapOut, int swapIn, Time startTime, Time endTime) {
         this.listOfProcesses = listOfProcesses;
         this.swapOut = swapOut;
         this.swapIn = swapIn;
@@ -18,30 +18,29 @@ public class Bloque {
         this.endTime = endTime;
     }
 
-    //Method to get listOfProcesses attribute
+    // Method to get listOfProcesses attribute
     public List<Process> getListOfProcesses() {
         return this.listOfProcesses;
     }
 
-    //Method to get swapOut attribute
+    // Method to get swapOut attribute
     public int getSwapOut() {
         return this.swapOut;
     }
 
-    //Method to get swapIn attribute
+    // Method to get swapIn attribute
     public int getSwapIn() {
         return this.swapIn;
     }
 
-    //Method to get startTime attribute
+    // Method to get startTime attribute
     public Time getStartTime() {
         return this.startTime;
     }
 
-    //Method to get endTime attribute
+    // Method to get endTime attribute
     public Time getEndTime() {
         return this.endTime;
     }
-
 
 }
