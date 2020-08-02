db35e8dbcd7bc29b1e82a57e22d56962d9907cf6
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2020-08-02 13:12:38 -0600

    Commented and refactored validation in Simulation

--- Simulation.java
+++ Simulation.java
@@ -11,10 +11,11 @@ public class Simulation {
     private int[] swaps = new int[2]; // 0 for out y 1 for in
     private boolean running;
 
-    Simulation(Scanner scanner, int PAGE_SIZE, int RAM_SIZE, int VRAM_SIZE, double timeOfSwap, double timeOfAccess, double timeOfFreeing, int swapMethod) {
+    Simulation(Scanner scanner, int PAGE_SIZE, int RAM_SIZE, int VRAM_SIZE, double timeOfSwap, double timeOfAccess,
+            double timeOfFreeing, int swapMethod) {
         this.scanner = scanner;
         try {
-            this.ram = new RAM(RAM_SIZE, PAGE_SIZE, timeOfSwap, timeOfAccess,timeOfFreeing);
+            this.ram = new RAM(RAM_SIZE, PAGE_SIZE, timeOfSwap, timeOfAccess, timeOfFreeing);
             this.vram = new VRAM(VRAM_SIZE, PAGE_SIZE);
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -24,15 +25,15 @@ public class Simulation {
         this.running = true;
     }
 
-    private int parseInt(String s){
+    private int parseInt(String s) {
         return Integer.parseInt(s);
     }
 
     /*
-        Ends Simulation by changing running value
-    */
-    
-    private void endSimulation(){
+     * Ends Simulation by changing running value
+     */
+
+    private void endSimulation() {
         ram.print();
         vram.print();
         System.out.println("GAMER OVER");
@@ -56,7 +57,7 @@ public class Simulation {
         System.out.println(s);
         switch (inputs[0]) {
             case "A":
-                accessVirtualAddress(parseInt(inputs[1]),parseInt(inputs[2]),parseInt(inputs[3]));
+                accessVirtualAddress(parseInt(inputs[1]), parseInt(inputs[2]), parseInt(inputs[3]));
                 break;
             case "C":
                 commentary(s.substring(2));
@@ -71,24 +72,23 @@ public class Simulation {
                 clean(parseInt(inputs[1]));
                 break;
             case "P":
-                createProcess(parseInt(inputs[2]),parseInt(inputs[1]));
+                createProcess(parseInt(inputs[2]), parseInt(inputs[1]));
                 break;
         }
     }
 
-    private void clean(int processId){
-        try{
-            swaps[0]+=ram.cleanProcess(processId, vram, time);
-            Process temp = ram.getDeadProcesses().get(ram.getDeadProcesses().size()-1);
+    private void clean(int processId) {
+        try {
+            swaps[0] += ram.cleanProcess(processId, vram, time);
+            Process temp = ram.getDeadProcesses().get(ram.getDeadProcesses().size() - 1);
             temp.printAddresses();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
     private void createProcess(int processId, int processSize) {
-        try{
+        try {
             int[] operationSwaps = ram.addProcess(processId, processSize, vram, time, 0);
             swaps[0] += operationSwaps[0];
             swaps[1] += operationSwaps[1];
@@ -97,13 +97,12 @@ public class Simulation {
         }
     }
 
-    private void accessVirtualAddress(int address, int processId, int readWrite){
-        try{
-            int[] operationSwaps = ram.access(address, processId, vram, time, swapMethod,readWrite);
+    private void accessVirtualAddress(int address, int processId, int readWrite) {
+        try {
+            int[] operationSwaps = ram.access(address, processId, vram, time, swapMethod, readWrite);
             swaps[0] += operationSwaps[0];
             swaps[1] += operationSwaps[1];
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
@@ -112,9 +111,13 @@ public class Simulation {
         String[] inputs = s.split(" ");
         switch (inputs[0]) {
             case "A":
-                if (inputs.length == 4 && isInt(inputs[1]) && isInt(inputs[2]) && (inputs[3].equals("0") | inputs[3].equals("1")))
-                    return true;
-                else
+                if (inputs.length == 4) { // Check size first to avoid outOfRangeException
+                    // Check correct data types
+                    if (isInt(inputs[1]) && isInt(inputs[2]) && (inputs[3].equals("0") | inputs[3].equals("1")))
+                        return true;
+                    else
+                        return false;
+                } else
                     return false;
             case "C":
                 if (s.length() > 3)
@@ -132,14 +135,20 @@ public class Simulation {
                 else
                     return false;
             case "L":
-                if (inputs.length == 2 && isInt(inputs[1]))
-                    return true;
-                else
+                if (inputs.length == 2) {
+                    if (isInt(inputs[1]))
+                        return true;
+                    else
+                        return false;
+                } else
                     return false;
             case "P":
-                if (inputs.length == 3 && isInt(inputs[1]) && isInt(inputs[2]))
-                    return true;
-                else
+                if (inputs.length == 3) {
+                    if (isInt(inputs[1]) && isInt(inputs[2]))
+                        return true;
+                    else
+                        return false;
+                } else
                     return false;
         }
         return false;
@@ -147,7 +156,7 @@ public class Simulation {
 
     public void startSimulation() {
         String command;
-        while(running && scanner.hasNextLine()){
+        while (running && scanner.hasNextLine()) {
             command = scanner.nextLine();
             if (checkValidString(command)) {
                 readEntry(command);
@@ -166,12 +175,12 @@ public class Simulation {
         }
     }
 
-    /* 
-        Prints a whole report of the block of processes
-    */
+    /*
+     * Prints a whole report of the block of processes
+     */
     public void report() {
         List<Process> deadProcesses = ram.getDeadProcesses();
-        if(deadProcesses.size() == 0){
+        if (deadProcesses.size() == 0) {
             System.out.println("Processes were not ended during this block");
             return;
         }
@@ -188,21 +197,21 @@ public class Simulation {
         System.out.println("Turnaround Por Proceso: ");
         double sumSeconds = 0;
         Time timeTemp;
-        for (Process process: deadProcesses) {
+        for (Process process : deadProcesses) {
             timeTemp = process.getTurnaround();
             System.out.print("\tProceso " + process.getId() + ": ");
             timeTemp.print();
-            sumSeconds += timeTemp.getMiliseconds()*0.001;
+            sumSeconds += timeTemp.getMiliseconds() * 0.001;
             sumSeconds += timeTemp.getSeconds();
-            sumSeconds += timeTemp.getMinutes()*60;
-            sumSeconds += timeTemp.getHours()*3600;
+            sumSeconds += timeTemp.getMinutes() * 60;
+            sumSeconds += timeTemp.getHours() * 3600;
         }
         average(deadProcesses.size(), sumSeconds);
-        
+
     }
-    
+
     private void average(int size, double sumSeconds) {
-        double average = sumSeconds/size;
+        double average = sumSeconds / size;
         Time timeTemp = new Time();
         timeTemp.addSeconds(average);
         System.out.println("Turnaround Promedio: ");
@@ -212,7 +221,7 @@ public class Simulation {
 
     private void numberOfPageFaults(List<Process> deadProcesses) {
         System.out.println("Page Faults");
-        for (Process process: deadProcesses) {
+        for (Process process : deadProcesses) {
             System.out.println("\tProceso " + process.getId() + ": " + process.getPageFaults());
         }
     }
