36ca9b5873f0c3fbc6e101e6422acba58ad2c1f3
Darfeikus <32723476+Darfeikus@users.noreply.github.com>
2020-08-03 21:16:28 -0500

    Modiyfied Main.java for selection

--- Main.java
+++ Main.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 class Main{
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
-        int method = 1; //0 for FIFO, 1 for LRU
+        System.out.println("Press 1 for FIFO, 2 for LRU. Default is FIFO");
+        int method = 0; //0 for FIFO, 1 for LRU
+        switch(s.nextLine()){
+            case "2":
+                System.out.println("LRU selected. Starting simulation...");
+                method=1;
+                break;
+            default:
+                System.out.println("FIFO selected. Starting simulation...");
+                break;
+        }
         //Simulation(Scanner, page_Size, RAM_Size,VRAM_Size,Time of swap, time of access, time of freeing)
         try{
             Simulation simulation = new Simulation(s, 16, 2048, 4096, 1, 0.1,0.1,method);
